aaedd61a876e6f0f9a1674475c99084aecc82849
ruizjcsc <ruizjonathan246@gmail.com>
2023-08-24 22:17:48 -0400

    http request for ip addresses using free api

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,14 +3,15 @@ import React,{Component} from 'react';
 //use uppercase for component names 
 import Banner from './bannerComponent';
 import ExhibitComponent from './exhibitComponent';
+import AddressRequest from './addressRequest'
 
 class App extends Component {
   render() {
     return(
       <div className='App'>
         <Banner bannerText = "Sextant"></Banner>
-        <ExhibitComponent name = "Exhibit One"><p>blah blah blah</p></ExhibitComponent>
-        <ExhibitComponent name = "Exhibit two"><p>blah blah</p></ExhibitComponent>
+        <ExhibitComponent name = "IPV4 address"><p><AddressRequest addressType = "ipv4"/></p></ExhibitComponent>
+        <ExhibitComponent name = "IPV6 address"><p><AddressRequest addressType = "ipv6"/></p></ExhibitComponent>
       </div>
     );
   }
---
+++ sextant/src/addressRequest.js
@@ -0,0 +1,40 @@
+import React,{ Component } from "react";
+import './addressRequest.css'
+
+class addressRequest extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            ipData: [],
+        };
+    }
+    componentDidMount() {
+        // below is the props to differentiate ipv4 and ipv6
+        // props short for porperties 
+        const {addressType} = this.props;
+        const url = addressType == "ipv6" ? 'https://api64.ipify.org?format=json' : 'https://api.ipify.org?format=json';
+        // this is the api call for ipv4
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("IP data received:", data);
+                this.setState({ ipData: data });
+            })
+            .catch((error) => console.log(error));
+        //why console not working?   
+    }
+
+    render(){
+        const {ipData} = this.state;
+
+        return(
+            <div className="addressRequest">
+                <p>IP Address</p>
+                {ipData ? <p>{ipData.ip}</p> : <p>Loading...</p>}
+
+            </div>
+        );
+    }
+}
+
+export default addressRequest;
